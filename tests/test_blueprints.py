4e054840207a20abdf7c7ed0eab27bf90d14e71e
Liam Bindle <liambindle@gmail.com>
2025-06-10 00:44:09 -0700

    tweaks

--- tests/test_blueprints.py
+++ tests/test_blueprints.py
@@ -23,20 +23,20 @@ def test_template_files():
     x = _DummyBlueprint()
     yielded_templates = set(src_dest_pair for src_dest_pair in x._template_list())
     assert ('volume/docker-compose.yaml', 'volume/docker-compose.yaml') in yielded_templates
-    assert ('module/cookiecutter.json', 'module/cookiecutter.json') in yielded_templates
+    assert ('package/cookiecutter.json', 'package/cookiecutter.json') in yielded_templates
 
 
-def test_render_postgres():
-    from vendorless.core import Volume
-    from vendorless.postgres import PostgresDatabase
-    from pathlib import Path
+# def test_render_postgres():
+#     from vendorless.core import Volume
+#     from vendorless.postgres import PostgresDatabase
+#     from pathlib import Path
 
-    v = Volume()
-    pg = PostgresDatabase(
-        volume_name=v.name,
-        data_path="/pgdata"
-    )
-    v.name = "pgdata_volume"
+#     v = Volume()
+#     pg = PostgresDatabase(
+#         volume_name=v.name,
+#         data_path="/pgdata"
+#     )
+#     v.name = "pgdata_volume"
 
-    root = Path('.build/')
-    Blueprint.render_stack(root)
+#     root = Path('.build/')
+#     Blueprint.render_stack(root)
