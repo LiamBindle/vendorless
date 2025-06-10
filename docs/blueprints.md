7f659d6fa45226954627851f5da00af0744adf05
Liam Bindle <liambindle@gmail.com>
2025-06-09 17:27:22 -0700

    added docs

---
+++ docs/blueprints.md
@@ -0,0 +1,2 @@
+
+::: vendorless.core.Volume
--- src/vendorless/core/volume.py
+++ src/vendorless/core/volume.py
@@ -5,7 +5,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Volume(Blueprint):
+    """
+    Represents a Docker volume configuration.
+    """
+
     name: str = parameter()
+    """The name of the Docker volume."""
 
     def _template_list(self) -> list[tuple[str, str]]:
         return [
