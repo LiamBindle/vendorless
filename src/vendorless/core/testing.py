2091ffd00b754b1a647c42c996f2d3d8cf45141a
Liam Bindle <liambindle@gmail.com>
2025-07-06 08:01:55 -0700

    added testing utilities

--- src/vendorless/core/testing.py
+++ src/vendorless/core/testing.py
@@ -1,7 +1,20 @@
-from click.testing import CliRunner
+from click.testing import CliRunner, Result
 from rich.prompt import Prompt
+from vendorless.core.cli import main
+
+from typing import Callable, Generator, Iterator
+
+from contextlib import contextmanager
 
 
 def stage_cli_prompt_responses(monkeypatch, inputs: list[str]):
-    inputs = iter(inputs)
-    monkeypatch.setattr(Prompt, "ask",  lambda *args, **kwargs: next(inputs))
+    inputs_iterator: Iterator[str] = iter(inputs)
+    monkeypatch.setattr(Prompt, "ask",  lambda *args, **kwargs: next(inputs_iterator))
+
+@contextmanager
+def temp_cli() -> Generator[Callable[[list[str]], Result], None, None]:
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        def run_args(args: list[str]) -> Result:
+            return runner.invoke(main, args)
+        yield run_args
